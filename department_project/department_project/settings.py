43a068a5e27575458bea806409be41a74cc10d5d
alex <leshastrunec@gmail.com>
2021-09-26 17:17:08 +0200

    add djnago-heroku

--- department_project/department_project/settings.py
+++ department_project/department_project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 
 # Heroku: Update database configuration from $DATABASE_URL.
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 # Password validation
